81568c12f9efb224d43198612221d941016ae1eb
Muhammad Fadli Nurhasan <fadli.nurhasan@go-jek.com>
2018-10-11 15:32:40 +0530

    [fadli/tara] allow app_group_secret as an alternative masterkey in vm setup

--- lib/fluent/plugin/out_barito_vm.rb
+++ lib/fluent/plugin/out_barito_vm.rb
@@ -7,10 +7,12 @@ module Fluent
   class BaritoVMOutput < BufferedOutput
 
     PLUGIN_NAME = "barito_vm"
-    
+
     Fluent::Plugin.register_output(PLUGIN_NAME, self)
 
     config_param :application_secret, :string, :default => nil
+    config_param :app_group_secret, :string, :default => nil
+    config_param :app_name, :string, :default => nil
     config_param :produce_url, :string, :default => ''
 
     # Overide from BufferedOutput
@@ -28,13 +30,20 @@ module Fluent
       chunk.msgpack_each do |tag, time, record|
         trail = Fluent::Plugin::ClientTrail.new(false)
         timber = Fluent::Plugin::TimberFactory::create_timber(tag, time, record, trail)
-        header = {content_type: :json, 'X-App-Secret' => @application_secret}
-        
+
+        if @application_secret.nil? or @application_secret.blank?
+          next if @app_group_secret.nil? or @app_name.nil?
+          header = {
+            content_type: :json,
+            'X-App-Group-Secret' => @app_group_secret,
+            'App-Name' => @app_name
+          }
+        else
+          header = {content_type: :json, 'X-App-Secret' => @application_secret}
+        end
+
         RestClient.post @produce_url, timber.to_json, header
       end
     end
-    
   end
-  
-
 end
